02ce07fd940e4ded94870ddad9c60532e47d8f7a
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-02-24 14:25:52 -0300

    Introdução as Rotas

--- FUNDAMENTOS DE ANGULAR/projeto_angular/projeto1/src/app/app.routes.ts
+++ FUNDAMENTOS DE ANGULAR/projeto_angular/projeto1/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Conteudo } from './componentes/conteudo/conteudo';
+import { Componente1 } from './componentes/componente1/componente1';
+import { Componente2 } from './componentes/componente2/componente2';
+import { Componente3 } from './componentes/componente3/componente3';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Conteudo},
+    {path:'link1', component: Componente1},
+    {path:'link2', component: Componente2},
+    {path:'link3', component: Componente3}
+];
