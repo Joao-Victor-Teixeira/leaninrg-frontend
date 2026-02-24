11da64822cc219018bbbf8c67fe7b58800aa7c81
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-02-24 13:54:17 -0300

    Criação de componentes

--- FUNDAMENTOS DE ANGULAR/projeto_angular/projeto1/src/app/app.ts
+++ FUNDAMENTOS DE ANGULAR/projeto_angular/projeto1/src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cabecalho } from './componentes/cabecalho/cabecalho';
+import { Rodape } from './componentes/rodape/rodape';
+import { Conteudo } from './componentes/conteudo/conteudo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    Cabecalho,
+    Conteudo,
+    Rodape],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ FUNDAMENTOS DE ANGULAR/projeto_angular/projeto1/src/app/componentes/conteudo/conteudo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conteudo',
+  imports: [],
+  templateUrl: './conteudo.html',
+  styleUrl: './conteudo.css',
+})
+export class Conteudo {
+
+}
