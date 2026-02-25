f415af6960eb868152c1504fc324a0e11173d6cf
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-02-25 17:46:19 -0300

    primeira parte de resolução de exercicios

--- FUNDAMENTOS DE ANGULAR/projeto_angular/projeto2/src/app/app.ts
+++ FUNDAMENTOS DE ANGULAR/projeto_angular/projeto2/src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './componentes/header/header';
+import { Body } from './componentes/body/body';
+import { Footer } from './componentes/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    Header,
+    Body,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
